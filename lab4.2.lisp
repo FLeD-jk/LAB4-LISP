9976102fcaefd674a4005e9fba82afd594d6e8f2
FLeD-jk <122401625+FLeD-jk@users.noreply.github.com>
2024-11-14 22:58:43 +0200

    Update lab4.2.lisp

--- lab4.2.lisp
+++ lab4.2.lisp
@@ -1,6 +1,6 @@
-(defun add-next-fn (&key transform)
-  (lambda (current next)
-    (let ((current-value (if transform (funcall transform current) current))
+(Defun Add-Next-Fn (&Key Transform)
+  (Lambda (Current Next)
+    (Let ((Current-Value (If Transform (Funcall Transform Current) current))
           (next-value (if (and next transform) (funcall transform next) next)))
       (cons current-value next-value))))
 
