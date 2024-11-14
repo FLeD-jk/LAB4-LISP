ea12a4194d3b16a9daa8651928c518124260fedd
FLeD-jk <122401625+FLeD-jk@users.noreply.github.com>
2024-11-14 23:23:28 +0200

    Update lab4.2.lisp

--- lab4.2.lisp
+++ lab4.2.lisp
@@ -10,7 +10,7 @@
           (append (cdr lst) (list nil)))) 
 
 (defun check-pair-elements-with-mapcar (name input expected &key transform)
-  "Execute shell-sorting-functional on input, compare result with expected and print comparison status"
+  "Execute  add-next-fn on input, compare result with expected and print comparison status"
   (let ((result (pair-elements-with-mapcar input :transform transform)))
     (format t "~:[~a failed! Expected: ~a Obtained: ~a~;~a passed! Expected: ~a Obtained: ~a~]~%"
             (equal result expected)
@@ -18,7 +18,7 @@
 
 
 (defun test-pair-elements-with-mapcar ()
-  (format t "Start testing shell-sorting-functional function~%")
+  (format t "Start testing  add-next-fn function~%")
   (check-pair-elements-with-mapcar "Сalling a function without a transform" '(1 2 3) '((1 . 2) (2 . 3) (3 . NIL)))
   (check-pair-elements-with-mapcar "Сalling a function with transform 1+" '(1 2 3) '((2 . 3) (3 . 4) (4 . NIL)) :transform #'1+)
   (check-pair-elements-with-mapcar "Сalling a function with transform 5+"  '(1 5 10 15) '((6 . 10) (10 . 15) (15 . 20) (20))  :transform (lambda (x) (+ x 5)))
