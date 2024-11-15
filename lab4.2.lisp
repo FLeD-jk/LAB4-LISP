299511b2d0e4316c9547c6381374d066531e669c
FLeD-jk <122401625+FLeD-jk@users.noreply.github.com>
2024-11-15 19:51:00 +0200

    Update lab4.2.lisp

--- lab4.2.lisp
+++ lab4.2.lisp
@@ -8,7 +8,7 @@
             (setf current (cons (cdr prev-element) nil))
             (setf prev-element current)))))) 
 
-(defun check-pair-elements-with-mapcar (name input expected &key transform)
+(defun check-add-next-fn (name input expected &key transform)
   "Execute  add-next-fn on input, compare result with expected and print comparison status"
   (let ((result (mapcar (add-next-fn :transform transform) input))) 
     (format t "~:[~a failed! Expected: ~a Obtained: ~a~;~a passed! Expected: ~a Obtained: ~a~]~%"
@@ -16,13 +16,13 @@
             name expected result)))
 
 
-(defun test-pair-elements-with-mapcar ()
+(defun test-check-add-next-fn ()
   (format t "Start testing add-next-fn function~%")
-  (check-pair-elements-with-mapcar "Сalling a function without a transform" '(1 2 3) '((1 . 2) (2 . 3) (3 . NIL)))
-  (check-pair-elements-with-mapcar "Сalling a function with transform 1+" '(1 2 3) '((2 . 3) (3 . 4) (4 . NIL)) :transform #'1+)
-  (check-pair-elements-with-mapcar "Сalling a function with transform 5+"  '(1 5 10 15) '((6 . 10) (10 . 15) (15 . 20) (20))  :transform (lambda (x) (+ x 5)))
-  (check-pair-elements-with-mapcar "Сalling a function with transform sqrt"  '(9 16 25) '((3 . 4) (4 . 5) (5)) :transform #'sqrt)
+  (check-add-next-fn "Сalling a function without a transform" '(1 2 3) '((1 . 2) (2 . 3) (3 . NIL)))
+  (check-add-next-fn "Сalling a function with transform 1+" '(1 2 3) '((2 . 3) (3 . 4) (4 . NIL)) :transform #'1+)
+  (check-add-next-fn "Сalling a function with transform 5+"  '(1 5 10 15) '((6 . 10) (10 . 15) (15 . 20) (20))  :transform (lambda (x) (+ x 5)))
+  (check-add-next-fn "Сalling a function with transform sqrt"  '(9 16 25) '((3 . 4) (4 . 5) (5)) :transform #'sqrt)
   (format t "EnD~%"))
 
 
-(test-pair-elements-with-mapcar)
+(test-check-add-next-fn)
